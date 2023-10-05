2c5fa78438cad27dc087181ddc086eed4c873ce9
paifuiii <63070014@kmitl.ac.th>
2023-10-05 21:03:55 +0700

    All note page 01box (loop grid and topic name not working) :art:

---
+++ components/BoxNote.js
@@ -0,0 +1,49 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function BoxNote({ notesText }) {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <View
+        style={{
+          borderColor: "#D9DADA",
+          width: "50%",
+          borderWidth: 2,
+          padding: 5,
+          borderRadius: 3,
+          justifyContent: "space-between",
+        }}
+      >
+        <View style={{ padding: 10 }}>
+          <View>
+            <Text
+              style={{ fontSize: 50, fontFamily: "Jura", color: "#8A97A0" }}
+            >
+              01
+            </Text>
+            <Text
+              style={{
+                fontSize: 17,
+                color: "#00213F",
+                fontFamily: "Jura",
+                fontWeight: "bold",
+              }}
+            >
+              Send email to ijon&igae.
+            </Text>
+          </View>
+          <Text
+            style={{
+              fontSize: 15,
+              color: "#B5B7B9",
+              fontFamily: "Jura",
+              marginTop: 26,
+            }}
+          >
+            26 Feb, 2022
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
