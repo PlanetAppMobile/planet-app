9a4154a883324aed8cc965780d58f65cf941146e
paifuiii <63070014@kmitl.ac.th>
2023-10-20 21:55:29 +0700

    forgotPR

--- screens/DetailsNote.js
+++ screens/DetailsNote.js
@@ -1,4 +1,12 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Modal,
+  StyleSheet,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import HeaderPic from "../assets/header-page.png";
 import BackIcon from "../assets/icons/back-icon.png";
@@ -8,7 +16,9 @@ import path from "../path";
 import Textarea from "react-native-textarea/src/Textarea";
 function DetailsNote({ route, navigation }) {
   const [onEdit, setOnEdit] = useState(false);
-  const { noteId } = route.params
+  const { noteId } = route.params;
+  const [modalVisible, setModalVisible] = useState(false);
+
   useEffect(() => {
     axios.get(`${path}/note/1/${noteId}`).then((res) => {
       console.log(res.data);
@@ -20,13 +30,16 @@ function DetailsNote({ route, navigation }) {
   function addLeadingZero(number) {
     let strNumber = number.toString();
     if (strNumber.length < 2) {
-      strNumber = '0' + strNumber;
+      strNumber = "0" + strNumber;
     }
     return strNumber;
   }
   function formatDate(inputDate) {
     const options = { year: "numeric", month: "short", day: "numeric" };
-    const formattedDate = new Date(inputDate).toLocaleDateString(undefined, options);
+    const formattedDate = new Date(inputDate).toLocaleDateString(
+      undefined,
+      options
+    );
     return formattedDate;
   }
   function handleEdit() {
@@ -34,12 +47,106 @@ function DetailsNote({ route, navigation }) {
   }
   return (
     <ScrollView style={{ backgroundColor: "#FBF7F0" }}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Image
+              style={{ width: "100%", height: 55 }}
+              source={HeaderPic}
+              resizeMode="contain"
+            />
+            <View style={{ padding: 0, justifyContent: "center" }}>
+              <Text style={styles.modalText}>RATE YOUR PROJECT</Text>
+              <Text
+                style={{
+                  fontFamily: "Jura",
+                  fontSize: 24,
+                  textAlign: "center",
+                  color: "#00213F",
+                }}
+              ></Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                width: "80%",
+                justifyContent: "space-between",
+                marginTop: 20,
+              }}
+            >
+              <View style={{ width: "50%" }}>
+                <TouchableOpacity
+                  onPress={() => {
+                    setModalVisible(false);
+                  }}
+                  style={{
+                    borderRadius: 5,
+                    height: 35,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderWidth: 2,
+                    borderColor: "#F08D6E",
+                    backgroundColor: "transparent",
+                    marginTop: 15,
+                    marginLeft: 5,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 15,
+                      color: "#F08D6E",
+                      fontFamily: "Copper",
+                    }}
+                  >
+                    CANCEL
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ width: "50%", marginLeft: 5 }}>
+                <TouchableOpacity
+                  style={{
+                    borderRadius: 5,
+                    height: 35,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "#F08D6E",
+                    marginTop: 15,
+                    marginRight: 5,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 15,
+                      color: "white",
+                      fontFamily: "Copper",
+                    }}
+                  >
+                    SUBMIT
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
       <Image
         style={{ width: "100%", height: 65 }}
         source={HeaderPic}
         resizeMode="contain"
       />
-      <TouchableOpacity onPress={() => { navigation.navigate("Note") }}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Note");
+        }}
+      >
         <Image
           style={{ width: "100%", height: 40, left: -175 }}
           source={BackIcon}
@@ -136,7 +243,8 @@ function DetailsNote({ route, navigation }) {
               </View>
             </View>
             <View style={{ padding: 15 }}>
-              <Textarea value={note.description}
+              <Textarea
+                value={note.description}
                 style={{
                   color: "#768592",
                   fontSize: 16,
@@ -165,4 +273,44 @@ function DetailsNote({ route, navigation }) {
     </ScrollView>
   );
 }
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(109, 109, 104, 0.5)",
+  },
+  modalView: {
+    backgroundColor: "#FBF7F0",
+    width: 330,
+    height: 300,
+    borderRadius: 20,
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  button: {
+    fontFamily: "Copper",
+    borderRadius: "0.1875rem",
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginTop: 10,
+    textAlign: "center",
+    fontSize: 28,
+    fontFamily: "JockeyOne",
+    letterSpacing: 3,
+  },
+});
 export default DetailsNote;
